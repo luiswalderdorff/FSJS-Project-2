fafea30382c1b879db0655b6df8a39c9f73ce95e
Luis Waderdorff <walderdorff@live.de>
2018-11-09 19:57:48 +0100

    Finished?

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ let selectedPage = 1;
 // only show 10 studentItems at a time
 
 function showPage (array, page) {
-  for (let i = 0; i < studentItems.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     // only show studentItems, that fall in the selected page range
     if ( i < page * 10 && i >= (page - 1) * 10) {
       array[i].style.display = "block";
@@ -26,8 +26,8 @@ showPage( studentItems, selectedPage );
 
 // create the buttons to change pages
 
-function appendPageLinks () {
-  const requiredPages = Math.ceil(studentItems.length / 10);
+function appendPageLinks (itemList) {
+  const requiredPages = Math.ceil(itemList.length / 10);
   // create the HTML
   const linkDiv = document.createElement("div");
   linkDiv.classList.add("pagination");
@@ -55,12 +55,12 @@ function appendPageLinks () {
       // change the selectedPage, for the showPage function
       selectedPage = parseInt(this.textContent);
       // re-call the showPage function
-      showPage( studentItems, selectedPage );
+      showPage( itemList, selectedPage );
     })
   }
 
 }
-appendPageLinks();
+appendPageLinks(studentItems);
 
 // Searchbar
 function createSearchbar () {
@@ -74,33 +74,79 @@ function createSearchbar () {
   // add event listener to the searchbar
   const searchButton = document.querySelector(".search-button");
   const searchInput = document.querySelector(".search-input");
+
   // search, if the button is clicked
   searchButton.addEventListener("click", function () {
     const searchText = searchInput.value.toLowerCase(); //toLowerCase, to make the search case insensitive
     // Look through all the studentItems and see if the search value is included in their names
     for ( let i = 0; i < studentItems.length; i++) {
-      const studentName = document.querySelectorAll(".student-details h3")[i].innerHTML.toLowerCase();
+      const studentNameText = document.querySelectorAll(".student-details h3")[i].innerHTML.toLowerCase();
+      const studentName = document.querySelectorAll(".student-details h3")[i];
        //indexOf The indexOf() method returns the position of the first occurrence of a specified value in a string.
        //This method returns -1 if the value to search for never occurs. "https://www.w3schools.com/jsref/jsref_indexof.asp"
-      if ( studentName.indexOf(searchText) != -1 ) {
-        studentItems[i].style.display = "block";
-      } else {
-        studentItems[i].style.display = "none";
-      }
-      }
-    })
-    // search, every time you enter a letter into the searchbar
-    searchInput.addEventListener("keyup", function () {
-      const searchText = searchInput.value.toLowerCase();
-      for ( let i = 0; i < studentItems.length; i++) {
-        const studentName = document.querySelectorAll(".student-details h3")[i].innerHTML.toLowerCase();
-        if ( studentName.indexOf(searchText) != -1 ) {
+      if ( studentNameText.indexOf(searchText) != -1 ) {
+        studentName.parentNode.parentNode.classList.add("student-item")
+        // only display the 10 items fitting the search, which also are on the selected page
+        if ( i < selectedPage * 10 && i >= (selectedPage - 1) * 10) {
           studentItems[i].style.display = "block";
         } else {
           studentItems[i].style.display = "none";
         }
+      } else {
+        studentItems[i].style.display = "none";
+        studentItems[i].classList.remove("student-item");
+      }
+    }
+    const studentItemsNew = document.querySelectorAll('.student-item');
+    // // if the page is empty, print a message to the page
+    if (studentItemsNew.length === 0 ) {
+      // only run if I haven't already printed No Results to the page
+      if (!document.querySelector(".no-results")) {
+      const noResults = document.createElement("h1");
+      noResults.classList.add("no-results");
+      noResults.innerHTML = `<h1>No Results</h1>`;
+      pageDiv.appendChild(noResults);
+      }
+    } else if (document.querySelector(".no-results")) {
+      const noResults = document.querySelector(".no-results");
+      noResults.parentNode.removeChild(noResults);
+    }
+    document.querySelector(".pagination").parentNode.removeChild(document.querySelector(".pagination"));
+    appendPageLinks(studentItemsNew);
+  })
+  // search on key press, same as above
+    searchInput.addEventListener("keyup", function () {
+      const searchText = searchInput.value.toLowerCase();
+      for ( let i = 0; i < studentItems.length; i++) {
+        const studentNameText = document.querySelectorAll(".student-details h3")[i].innerHTML.toLowerCase();
+        const studentName = document.querySelectorAll(".student-details h3")[i];
+        if ( studentNameText.indexOf(searchText) != -1 ) {
+          studentName.parentNode.parentNode.classList.add("student-item")
+          if ( i < selectedPage * 10 && i >= (selectedPage - 1) * 10) {
+            studentItems[i].style.display = "block";
+          } else {
+            studentItems[i].style.display = "none";
+          }
+        } else {
+          studentItems[i].style.display = "none";
+          studentItems[i].classList.remove("student-item");
         }
-      })
+      }
+      const studentItemsNew = document.querySelectorAll('.student-item');
+      if (studentItemsNew.length === 0 ) {
+        if (!document.querySelector(".no-results")) {
+        const noResults = document.createElement("h1");
+        noResults.classList.add("no-results");
+        noResults.innerHTML = `<h1>No Results</h1>`;
+        pageDiv.appendChild(noResults);
+        }
+      } else if (document.querySelector(".no-results")) {
+        const noResults = document.querySelector(".no-results");
+        noResults.parentNode.removeChild(noResults);
+      }
+      document.querySelector(".pagination").parentNode.removeChild(document.querySelector(".pagination"));
+      appendPageLinks(studentItemsNew);
+    })
   }
 
 
